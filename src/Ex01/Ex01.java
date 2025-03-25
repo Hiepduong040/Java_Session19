197afa06bbdd22c532a05ecd0b6ebf9cf4ce33a4
Hiepduong040 <147359314+Hiepduong040@users.noreply.github.com>
2025-03-26 05:22:53 +0700

    Ex01.java

--- src/Ex01/Ex01.java
+++ src/Ex01/Ex01.java
@@ -1,23 +1,23 @@
-package Ex01;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Ex01 {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
         List<String> listFilter = new ArrayList<>();
+
+  
         list.add("Java");
         list.add("Python");
         list.add("C++");
         list.add("");
-        for(String s : list) {
-            if(s.isEmpty()) {
+        Predicate<String> isEmptyString = String::isEmpty;
+        for (String s : list) {
+            if (isEmptyString.test(s)) {
                 listFilter.add(s);
             } else {
-                System.out.println(s);
-            }
-
+                System.out.println(s); 
         }
     }
 }
